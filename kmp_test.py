eae4842bc4a4f1599a4f72567ef935cb4cc6a1bf
SungMinCho <tjdals4565@gmail.com>
2019-11-30 13:32:21 +0900

    Measure time

--- kmp_test.py
+++ kmp_test.py
@@ -1,7 +1,9 @@
 from kmp import *
+
 from tqdm import trange
 
 import random
+import time
 
 
 def naive_match(text, pattern):
@@ -19,21 +21,43 @@ def naive_match(text, pattern):
             yield i
 
 
-def test_single(text, pattern):
+def test_single_correctness(text, pattern):
     naive_matches = list(naive_match(text, pattern))
     kmp_matches = list(kmp_match(text, pattern))
     assert naive_matches == kmp_matches, '{} != {}'.format(naive_matches, kmp_matches)
     # print('{} in {} : {}'.format(pattern, text, naive_matches))
 
 
-def test():
+def test_correctness():
     n, m = 100, 10
     for _ in trange(100000):
         # index starts from 1
         text = [None] + [random.randint(1, 10) for _ in range(n)]
         pattern = [None] + [random.randint(1, 10) for _ in range(m)]
-        test_single(text, pattern)
+        test_single_correctness(text, pattern)
 
+
+def test_single_time(n, m, repeat=10):
+    total_time = 0
+    for _ in range(repeat):
+        text = [None] + [random.randint(1, 10) for _ in range(n)]
+        pattern = [None] + [random.randint(1, 10) for _ in range(m)]
+        start = time.time()
+        matches = list(kmp_match(text, pattern))
+        total_time += time.time() - start
+    return total_time / repeat
+
+
+def test_time():
+    # ns = [10, 100, 1000, 10000, 100000, 1000000, 10000000]
+    # ms = [10, 100, 1000, 10000]
+    ns = [1000000 * (i+1) for i in range(5)]
+    ms = [10000 * (i+1) for i in range(5)]
+
+    for n in ns:
+        for m in ms:
+            print('{} {} {:.2f}secs'.format(n, m, test_single_time(n, m, 3)))
 
 if __name__ == "__main__":
-    test()
+    # test_correctness()
+    test_time()
